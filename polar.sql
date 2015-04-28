ac04dc9ea938554de177915f82f5499464ea1bda
dlake413 <biglake@udel.edu>
2015-04-28 13:00:07 +0000

    cloud dump

---
+++ polar.sql
@@ -0,0 +1,179 @@
+-- MySQL dump 10.13  Distrib 5.5.40, for debian-linux-gnu (x86_64)
+--
+-- Host: 0.0.0.0    Database: polar
+-- ------------------------------------------------------
+-- Server version	5.5.40-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `crew_members`
+--
+
+DROP TABLE IF EXISTS `crew_members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `crew_members` (
+  `cid` int(11) NOT NULL,
+  `eid` int(11) NOT NULL,
+  `ranks` varchar(30) NOT NULL,
+  `died_on_expedition` tinyint(4) DEFAULT NULL,
+  KEY `cid` (`cid`),
+  KEY `eid` (`eid`),
+  CONSTRAINT `crew_members_ibfk_1` FOREIGN KEY (`cid`) REFERENCES `people` (`id`),
+  CONSTRAINT `crew_members_ibfk_2` FOREIGN KEY (`eid`) REFERENCES `expedition` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `crew_members`
+--
+
+LOCK TABLES `crew_members` WRITE;
+/*!40000 ALTER TABLE `crew_members` DISABLE KEYS */;
+/*!40000 ALTER TABLE `crew_members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `expedition`
+--
+
+DROP TABLE IF EXISTS `expedition`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `expedition` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `source` int(11) NOT NULL,
+  `destination` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `destination` (`destination`),
+  KEY `source` (`source`),
+  CONSTRAINT `expedition_ibfk_1` FOREIGN KEY (`destination`) REFERENCES `location` (`id`),
+  CONSTRAINT `expedition_ibfk_2` FOREIGN KEY (`source`) REFERENCES `location` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `expedition`
+--
+
+LOCK TABLES `expedition` WRITE;
+/*!40000 ALTER TABLE `expedition` DISABLE KEYS */;
+/*!40000 ALTER TABLE `expedition` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `expedition_ships`
+--
+
+DROP TABLE IF EXISTS `expedition_ships`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `expedition_ships` (
+  `sid` int(11) NOT NULL,
+  `eid` int(11) NOT NULL,
+  KEY `sid` (`sid`),
+  KEY `eid` (`eid`),
+  CONSTRAINT `expedition_ships_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `ships` (`id`),
+  CONSTRAINT `expedition_ships_ibfk_2` FOREIGN KEY (`eid`) REFERENCES `expedition` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `expedition_ships`
+--
+
+LOCK TABLES `expedition_ships` WRITE;
+/*!40000 ALTER TABLE `expedition_ships` DISABLE KEYS */;
+/*!40000 ALTER TABLE `expedition_ships` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `location`
+--
+
+DROP TABLE IF EXISTS `location`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `location` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `location`
+--
+
+LOCK TABLES `location` WRITE;
+/*!40000 ALTER TABLE `location` DISABLE KEYS */;
+/*!40000 ALTER TABLE `location` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `people`
+--
+
+DROP TABLE IF EXISTS `people`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `people` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `people`
+--
+
+LOCK TABLES `people` WRITE;
+/*!40000 ALTER TABLE `people` DISABLE KEYS */;
+/*!40000 ALTER TABLE `people` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ships`
+--
+
+DROP TABLE IF EXISTS `ships`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ships` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ships`
+--
+
+LOCK TABLES `ships` WRITE;
+/*!40000 ALTER TABLE `ships` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ships` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-04-28 12:59:48
